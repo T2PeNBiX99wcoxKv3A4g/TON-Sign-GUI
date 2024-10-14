1aaacf7f796bf91e034320b8aa1daf0db999f5e2
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-15 04:48:09 +0800

    fix(Test): Add test code

---
+++ src/test/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/test/isAlternatePatternTest.kt
@@ -0,0 +1,20 @@
+package io.github.t2penbix99wcoxkv3a4g.tonsign.test
+
+import io.github.t2penbix99wcoxkv3a4g.tonsign.roundType.GuessRoundType
+
+fun main() {
+    val test = listOf(
+        GuessRoundType.Classic,
+        GuessRoundType.Classic,
+        GuessRoundType.Special,
+        GuessRoundType.Classic,
+        GuessRoundType.Classic,
+        GuessRoundType.Special,
+        GuessRoundType.Classic,
+        GuessRoundType.Classic
+    )
+
+    var test2 = test.takeLast(6).count { it == GuessRoundType.Special }
+
+    Utils.logger.info { "Test a $test $test2" }
+}
