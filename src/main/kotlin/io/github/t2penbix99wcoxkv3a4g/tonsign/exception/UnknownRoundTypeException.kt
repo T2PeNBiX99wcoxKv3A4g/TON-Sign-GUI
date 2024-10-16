24d90d460e8f38e8061a9a901e3e6ad2b617a272
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-16 17:30:50 +0800

    fix(Exception): Add new `Exception`

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/exception/UnknownRoundTypeException.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/exception/UnknownRoundTypeException.kt
@@ -1,7 +1,7 @@
 package io.github.t2penbix99wcoxkv3a4g.tonsign.exception
 
 @Suppress("unused")
-class WrongRecentRoundException: Exception {
+class UnknownRoundTypeException: Exception {
     constructor() : super()
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable) : super(message, cause)
