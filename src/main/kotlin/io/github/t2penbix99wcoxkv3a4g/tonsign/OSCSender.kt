52036c405a4ff67f234e2d993e9564ab4c5cc00d
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-19 20:21:18 +0800

    fix(OSCSender): Add `sendParam`

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/OSCSender.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/OSCSender.kt
@@ -5,15 +5,16 @@ import com.illposed.osc.transport.OSCPortOut
 import io.github.t2penbix99wcoxkv3a4g.tonsign.logger.Logger
 import java.net.InetSocketAddress
 
+@Suppress("unused")
 object OSCSender {
     private const val PORT = 9000
     private const val IP = "127.0.0.1"
 
     val oscClient = OSCPortOut(InetSocketAddress(IP, PORT))
 
-    fun send(bool: Boolean) {
-        send("/avatar/parameters/TON_Sign", bool)
-    }
+    fun send(bool: Boolean) = send("/avatar/parameters/TON_Sign", bool)
+
+    fun <T> sendParam(param: String, input: T) = send("/avatar/parameters/$param", input)
 
     fun <T> send(addres: String, input: T) {
         val message = OSCMessage(addres, mutableListOf(input))
