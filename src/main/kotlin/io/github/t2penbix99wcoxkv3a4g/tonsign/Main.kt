beb0721ec01b5fd6f39778a01befb6fd0ec046aa
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-21 04:28:04 +0800

    fix(MainKt): Change some gui look

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/Main.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/Main.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -20,22 +19,42 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogWindow
+import androidx.compose.ui.window.Tray
+import androidx.compose.ui.window.TrayState
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberNotification
+import androidx.compose.ui.window.rememberTrayState
+import io.github.t2penbix99wcoxkv3a4g.tonsign.coroutineScope.GUIScope
+import io.github.t2penbix99wcoxkv3a4g.tonsign.coroutineScope.LogicScope
+import io.github.t2penbix99wcoxkv3a4g.tonsign.logger.Logger
+import io.github.t2penbix99wcoxkv3a4g.tonsign.manager.ConfigManager
+import io.github.t2penbix99wcoxkv3a4g.tonsign.roundType.GuessRoundType
 import io.github.t2penbix99wcoxkv3a4g.tonsign.watcher.LogWatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import io.github.t2penbix99wcoxkv3a4g.tonsign.watcher.VRChatWatcher
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+val logicScope = LogicScope()
+val guiScope = GUIScope()
+var readerIsStarted = false
+var tickIsStarted = false
+var runningTime = 0
+var needToWait = false
+var reader: LogWatcher? = null
+var lastPrediction = GuessRoundType.NIL
 
 @Composable
 @Preview
-fun app() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun app(trayState: TrayState) {
+    var lastPrediction by remember { mutableStateOf(lastPrediction) }
+    var isWaitingVRChat by remember { mutableStateOf(VRChatWatcher.isWaitingVRChat) }
 
     MaterialTheme {
         Box(
@@ -67,11 +86,28 @@ fun app() {
                 ) {
                     Text("Send Chat Test")
                 }
+
                 Box(
                     Modifier.fillMaxSize()
-                        .padding(end = 12.dp, bottom = 12.dp)
+                        .padding(start = 12.dp, end = 12.dp, bottom = 12.dp)
                 ) {
-                    textBox(text)
+                    if (isWaitingVRChat) {
+                        textBox("VRChat is not running")
+                    }
+
+                    if (reader != null) {
+                        if (reader!!.nextRoundGuess != GuessRoundType.NIL) {
+                            val next = reader!!.nextRoundGuess
+
+                            if (next != lastPrediction) {
+                                lastPrediction = next
+
+                                textBox("Next Round is $next")
+                            }
+                        }
+
+                        textBox("Recent Rounds: ${reader!!.getRecentRoundsLog(ConfigManager.config.maxRecentRounds)}")
+                    }
                 }
             }
         }
@@ -82,8 +118,8 @@ fun app() {
 fun textBox(text: String = "Item") {
     Box(
         Modifier.height(32.dp)
-            .width(400.dp)
-            .background(Color(200, 0, 0, 20))
+            .fillMaxWidth()
+            .background(Color(0, 0, 0, 20))
             .padding(start = 10.dp),
         contentAlignment = Alignment.CenterStart
     ) {
@@ -91,15 +127,115 @@ fun textBox(text: String = "Item") {
     }
 }
 
-val reader = LogWatcher.Companion.Default
+private fun startReader(trayState: TrayState) {
+    if (readerIsStarted)
+        return
 
+    logicScope.launch {
+        while (true) {
+            runCatching {
+                if (!VRChatWatcher.isVRChatRunning()) {
+                    if (!needToWait)
+                        needToWait = true
+                    VRChatWatcher.checkVRChatLoop()
+                }
 
-fun main() = application {
-    scope.launch {
-        reader.monitorRoundType()
+                if (needToWait) {
+                    Logger.info("log.wait_until_join_game")
+                    delay(60000)
+                }
+
+                runningTime++
+                reader = LogWatcher.Default
+                reader!!.monitorRoundType()
+            }.getOrElse {
+                Logger.error(it, "exception.something_is_not_right", it.message!!)
+                needToWait = false
+            }
+        }
     }
 
-    Window(onCloseRequest = ::exitApplication, title = Utils.TITLE) {
-        app()
+    readerIsStarted = true
+}
+
+fun main() = application {
+    var isOpen by remember { mutableStateOf(true) }
+    var isAskingToClose by remember { mutableStateOf(false) }
+    var isVisible by remember { mutableStateOf(true) }
+    val trayState = rememberTrayState()
+    val notification = rememberNotification("Notification", "Message from MyApp!")
+    var lastPrediction by remember { mutableStateOf(lastPrediction) }
+
+    startReader(trayState)
+
+    if (isOpen) {
+        Tray(
+            state = trayState,
+            icon = TrayIcon,
+            tooltip = Utils.TITLE,
+            onAction = {
+                if (!isVisible)
+                    isVisible = true
+            },
+            menu = {
+                MaterialTheme {
+                    Item(
+                        "Send notification",
+                        onClick = {
+                            trayState.sendNotification(notification)
+                        }
+                    )
+                    Item(
+                        "Exit",
+                        onClick = {
+                            if (!isVisible)
+                                isVisible = true
+                            isAskingToClose = true
+                        }
+                    )
+
+                    if (reader != null && reader!!.nextRoundGuess != GuessRoundType.NIL) {
+                        val next = reader!!.nextRoundGuess
+
+                        if (next != lastPrediction) {
+                            lastPrediction = next
+
+                            val nextNotification = rememberNotification("Next Round", "Next round is $next")
+                            trayState.sendNotification(nextNotification)
+                        }
+                    }
+                }
+            }
+        )
+
+        Window(onCloseRequest = { isVisible = false }, visible = isVisible, title = Utils.TITLE) {
+            app(trayState)
+
+            if (isAskingToClose) {
+                DialogWindow(
+                    onCloseRequest = { isAskingToClose = false },
+                    title = "Test"
+                ) {
+                    Button(
+                        onClick = { isOpen = false }
+                    ) {
+                        Text("Yes")
+                    }
+//                    Button(
+//                        onClick = { isAskingToClose = false }
+//                    ) {
+//                        Text("No")
+//                    }
+                }
+            }
+        }
+    }
+}
+
+object TrayIcon : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color(0xFFFFA500))
     }
 }
