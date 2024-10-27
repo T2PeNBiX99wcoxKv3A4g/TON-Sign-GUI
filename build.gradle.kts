68ba166b98254131d9da80e38c47c19edbb64d78
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-27 10:35:32 +0800

    fix(Gradle): Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            modules("java.naming")
             packageName = "TON-Sign"
             packageVersion = "1.0.0"
         }
