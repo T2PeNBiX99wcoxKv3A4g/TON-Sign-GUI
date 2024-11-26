68b20d8a8687a2e7cca2100aeff22a01a8201d7c
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-11-26 23:15:08 +0800

    fix(LoggerEX): Return `Job`

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/logger/LoggerEX.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/logger/LoggerEX.kt
@@ -4,111 +4,112 @@ package io.github.t2penbix99wcoxkv3a4g.tonsign.logger
 
 import io.github.t2penbix99wcoxkv3a4g.tonsign.ex.toStringSafe
 import io.github.t2penbix99wcoxkv3a4g.tonsign.manager.LanguageManager
+import kotlinx.coroutines.Job
 
-inline fun <reified T : Any> Logger.debug(noinline message: () -> Any?) {
+inline fun <reified T : Any> Logger.debug(noinline message: () -> Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.debug({ "[%s] $msg" }, T::class.simpleName)
+    return this.debug({ "[%s] $msg" }, T::class.simpleName)
 }
 
-inline fun <reified T : Any> Logger.info(noinline message: () -> Any?) {
+inline fun <reified T : Any> Logger.info(noinline message: () -> Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.info({ "[%s] $msg" }, T::class.simpleName)
+    return this.info({ "[%s] $msg" }, T::class.simpleName)
 }
 
-inline fun <reified T : Any> Logger.warn(noinline message: () -> Any?) {
+inline fun <reified T : Any> Logger.warn(noinline message: () -> Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.warn({ "[%s] $msg" }, T::class.simpleName)
+    return this.warn({ "[%s] $msg" }, T::class.simpleName)
 }
 
-inline fun <reified T : Any> Logger.error(noinline message: () -> Any?) {
+inline fun <reified T : Any> Logger.error(noinline message: () -> Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.error({ "[%s] $msg" }, T::class.simpleName)
+    return this.error({ "[%s] $msg" }, T::class.simpleName)
 }
 
-inline fun <reified T : Any> Logger.debug(noinline message: () -> Any?, vararg objects: Any?) {
+inline fun <reified T : Any> Logger.debug(noinline message: () -> Any?, vararg objects: Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.debug({ "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.debug({ "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
-inline fun <reified T : Any> Logger.info(noinline message: () -> Any?, vararg objects: Any?) {
+inline fun <reified T : Any> Logger.info(noinline message: () -> Any?, vararg objects: Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.info({ "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.info({ "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
-inline fun <reified T : Any> Logger.warn(noinline message: () -> Any?, vararg objects: Any?) {
+inline fun <reified T : Any> Logger.warn(noinline message: () -> Any?, vararg objects: Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.warn({ "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.warn({ "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
-inline fun <reified T : Any> Logger.error(noinline message: () -> Any?, vararg objects: Any?) {
+inline fun <reified T : Any> Logger.error(noinline message: () -> Any?, vararg objects: Any?): Job {
     val msg = LanguageManager.get(message.toStringSafe())
-    this.error({ "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.error({ "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
 inline fun <reified T : Any> Logger.debug(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.debug(throwable, { "[%s] $msg" }, T::class.simpleName)
+    return this.debug(throwable, { "[%s] $msg" }, T::class.simpleName)
 }
 
 inline fun <reified T : Any> Logger.info(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.info(throwable, { "[%s] $msg" }, T::class.simpleName)
+    return this.info(throwable, { "[%s] $msg" }, T::class.simpleName)
 }
 
 inline fun <reified T : Any> Logger.warn(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.warn(throwable, { "[%s] $msg" }, T::class.simpleName)
+    return this.warn(throwable, { "[%s] $msg" }, T::class.simpleName)
 }
 
 inline fun <reified T : Any> Logger.error(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.error(throwable, { "[%s] $msg" }, T::class.simpleName)
+    return this.error(throwable, { "[%s] $msg" }, T::class.simpleName)
 }
 
 inline fun <reified T : Any> Logger.debug(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?,
     vararg objects: Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.debug(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.debug(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
 inline fun <reified T : Any> Logger.info(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?,
     vararg objects: Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.info(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.info(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
 inline fun <reified T : Any> Logger.warn(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?,
     vararg objects: Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.warn(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.warn(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
 }
 
 inline fun <reified T : Any> Logger.error(
     throwable: Throwable?,
     noinline message: (Throwable?) -> Any?,
     vararg objects: Any?
-) {
+): Job {
     val msg = LanguageManager.get(message.toStringSafe(throwable))
-    this.error(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
+    return this.error(throwable, { "[%s] $msg" }, T::class.simpleName, *objects)
 }
