e26ebc4da22c90e46e43b4746848bf7c4241af66
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-11-26 23:12:32 +0800

    fix(Logger): Log inside `CoroutineScope`

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/logger/Logger.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/logger/Logger.kt
@@ -1,52 +1,79 @@
 package io.github.t2penbix99wcoxkv3a4g.tonsign.logger
 
 import io.github.t2penbix99wcoxkv3a4g.tonsign.Utils
+import io.github.t2penbix99wcoxkv3a4g.tonsign.coroutineScope.LoggerScope
 import io.github.t2penbix99wcoxkv3a4g.tonsign.ex.toStringSafe
 import io.github.t2penbix99wcoxkv3a4g.tonsign.manager.LanguageManager
+import kotlinx.coroutines.launch
 
 @Suppress("unused")
 object Logger {
-    fun debug(message: () -> Any?) = Utils.logger.debug { LanguageManager.get(message.toStringSafe()) }
+    fun debug(message: () -> Any?) = LoggerScope.launch {
+        Utils.logger.debug { LanguageManager.get(message.toStringSafe()) }
+    }
 
-    fun info(message: () -> Any?) = Utils.logger.info { LanguageManager.get(message.toStringSafe()) }
+    fun info(message: () -> Any?) = LoggerScope.launch {
+        Utils.logger.info { LanguageManager.get(message.toStringSafe()) }
+    }
 
-    fun warn(message: () -> Any?) = Utils.logger.warn { LanguageManager.get(message.toStringSafe()) }
+    fun warn(message: () -> Any?) = LoggerScope.launch {
+        Utils.logger.warn { LanguageManager.get(message.toStringSafe()) }
+    }
 
-    fun error(message: () -> Any?) = Utils.logger.error { LanguageManager.get(message.toStringSafe()) }
+    fun error(message: () -> Any?) = LoggerScope.launch {
+        Utils.logger.error { LanguageManager.get(message.toStringSafe()) }
+    }
 
-    fun debug(message: () -> Any?, vararg objects: Any?) =
+    fun debug(message: () -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.debug { LanguageManager.get(message.toStringSafe(), *objects) }
+    }
 
-    fun info(message: () -> Any?, vararg objects: Any?) =
+    fun info(message: () -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.info { LanguageManager.get(message.toStringSafe(), *objects) }
+    }
 
-    fun warn(message: () -> Any?, vararg objects: Any?) =
+    fun warn(message: () -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.warn { LanguageManager.get(message.toStringSafe(), *objects) }
+    }
 
-    fun error(message: () -> Any?, vararg objects: Any?) =
+    fun error(message: () -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.error { LanguageManager.get(message.toStringSafe(), *objects) }
+    }
 
-    fun debug(throwable: Throwable?, message: (Throwable?) -> Any?) =
+    fun debug(throwable: Throwable?, message: (Throwable?) -> Any?) = LoggerScope.launch {
         Utils.logger.debug(throwable) { LanguageManager.get(message.toStringSafe(throwable)) }
+    }
 
-    fun info(throwable: Throwable?, message: (Throwable?) -> Any?) =
+    fun info(throwable: Throwable?, message: (Throwable?) -> Any?) = LoggerScope.launch {
         Utils.logger.info(throwable) { LanguageManager.get(message.toStringSafe(throwable)) }
+    }
 
-    fun warn(throwable: Throwable?, message: (Throwable?) -> Any?) =
+    fun warn(throwable: Throwable?, message: (Throwable?) -> Any?) = LoggerScope.launch {
         Utils.logger.warn(throwable) { LanguageManager.get(message.toStringSafe(throwable)) }
+    }
 
-    fun error(throwable: Throwable?, message: (Throwable?) -> Any?) =
+    fun error(throwable: Throwable?, message: (Throwable?) -> Any?) = LoggerScope.launch {
         Utils.logger.error(throwable) { LanguageManager.get(message.toStringSafe(throwable)) }
+    }
 
-    fun debug(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) =
-        Utils.logger.debug(throwable) { LanguageManager.get(message.toStringSafe(throwable), *objects) }
+    fun debug(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) = LoggerScope.launch {
+        Utils.logger.debug(throwable) {
+            LanguageManager.get(
+                message.toStringSafe(throwable),
+                *objects
+            )
+        }
+    }
 
-    fun info(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) =
+    fun info(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.info(throwable) { LanguageManager.get(message.toStringSafe(throwable), *objects) }
+    }
 
-    fun warn(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) =
+    fun warn(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.warn(throwable) { LanguageManager.get(message.toStringSafe(throwable), *objects) }
+    }
 
-    fun error(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) =
+    fun error(throwable: Throwable?, message: (Throwable?) -> Any?, vararg objects: Any?) = LoggerScope.launch {
         Utils.logger.error(throwable) { LanguageManager.get(message.toStringSafe(throwable), *objects) }
+    }
 }
