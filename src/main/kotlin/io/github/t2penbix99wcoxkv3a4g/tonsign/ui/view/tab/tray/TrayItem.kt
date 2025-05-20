0fb38559a26c34ba85fe30c046efe22dc5be9adc
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-05-20 21:49:32 +0800

    fix(TrayRoundDatas): Fix localization issue

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/ui/view/tab/tray/TrayItem.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/ui/view/tab/tray/TrayItem.kt
@@ -2,7 +2,7 @@ package io.github.t2penbix99wcoxkv3a4g.tonsign.ui.view.tab.tray
 
 import io.github.t2penbix99wcoxkv3a4g.tonsign.ui.view.tab.TabBodyBase
 
-abstract class TrayItem() {
+abstract class TrayItem {
     abstract val label: String
     open val isEnabled
         get() = true
--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/ui/view/tab/tray/TrayRoundDatas.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/tonsign/ui/view/tab/tray/TrayRoundDatas.kt
@@ -3,7 +3,7 @@ package io.github.t2penbix99wcoxkv3a4g.tonsign.ui.view.tab.tray
 import io.github.t2penbix99wcoxkv3a4g.tonsign.ui.view.tab.TabBodyBase
 import io.github.t2penbix99wcoxkv3a4g.tonsign.ui.view.tab.TabBodyRoundDatas
 
-class TrayRoundDatas(override val label: String = "Round Data Viewer") : TrayItem() {
+class TrayRoundDatas(override val label: String = "gui.tab.title.round_datas") : TrayItem() {
     override fun onClick(tabBody: TabBodyBase) {
         if (tabBody !is TabBodyRoundDatas) return
         tabBody.internalIsOnTop.value = true
